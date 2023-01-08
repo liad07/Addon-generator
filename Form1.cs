09465694e5ff8372b62de841e17445a16f031b3e
liadk07 <91607695+liad07@users.noreply.github.com>
2023-01-08 21:32:26 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Addon_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string verison = textBox2.Text;
+            string script = richTextBox1.Text;
+            System.IO.Directory.CreateDirectory(name+"-"+verison);
+            String json = "{\n\"manifest_version\":2,\n\"name\":\" "+name+"\",\n \"version\": \""+verison+"\",\n \"content_scripts\": [ \n{ \n   \"matches\": [\"<all_urls>\"],\n\"js\": [\""+name+".js\"]}\n]\n}";
+            File.WriteAllText(@"" + name + "-" + verison + "/" + "manifest.json", json);
+            File.WriteAllText(@"" + name + "-" + verison + "/" + name+".js", script);
+
+        }
+    }
+}
